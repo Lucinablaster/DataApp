37182c476df0d84d09fbcf55a0161b7f2fa0e800
Popcornblaster <lucinablaster@gmail.com>
2018-11-26 12:12:55 -0700

    Initial Commit

---
+++ DataApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataApp
+//
+//  Created by Baugh, Tyler on 11/26/18.
+//  Copyright © 2018 Baugh, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
